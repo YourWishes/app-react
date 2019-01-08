03f4f7941a8e2778dd6d080d12ac24aa9b7b1149
Dominic Masters <dominic@domsplace.com>
2019-01-08 20:08:11 +1100

    Fixed out directory.

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ export * from './compiler/';
 //TODO: I need to be able to get these folders from the tsconfig somehow
 import * as path from 'path';
 export const AppRoot = path.resolve(`./`);//The directory the CLI is running from
-export const ReactBase = path.resolve(`${AppRoot}/dist`);//The Dist folder root (has public and private)
+export const ReactBase = path.resolve(`${AppRoot}/dist/public`);//The Dist folder root
 export const ReactSource = path.resolve(`${AppRoot}/public`);//The source folder (has index.html)
